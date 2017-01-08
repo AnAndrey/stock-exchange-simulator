e4558b878eefcef0012cb9329fddeaa2c36912a7
AnAndrey <an.andrey.g@gmail.com>
2017-01-08 19:46:45 +0300

    Ignore certificate errors.

--- WebClientApplication/Global.asax.cs
+++ WebClientApplication/Global.asax.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +19,11 @@ namespace WebClientApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate,
+                     X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            { return true; };
         }
     }
 }
