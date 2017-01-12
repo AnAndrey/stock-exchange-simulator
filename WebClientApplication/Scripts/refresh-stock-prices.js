cf68e597d685eb26c21fae59af37ae52f51d13c6
AnAndrey <an.andrey.g@gmail.com>
2017-01-12 17:06:18 +0300

    Move script block out of HTML code.

--- WebClientApplication/Scripts/refresh-stock-prices.js
+++ WebClientApplication/Scripts/refresh-stock-prices.js
@@ -1,1 +1,7 @@
-﻿
+﻿$('document').ready(function() {
+    var url = "StockPrices/Home";
+    var notifications = $("#stock-prices-table");
+    setInterval(function () {
+        notifications.load(url);
+    }, 3000);
+})
